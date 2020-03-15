09071a25950281641dad100d09804681b26d5513
Manan <gouhari21@hotmail.com>
2020-03-16 03:12:48 +0530

    Added tooltip navigation menus to sites and metrics, theme menu is left right now.

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 window.addEventListener('DOMContentLoaded', () =>{
-    tippy('#sites', {
-        content: 'THIS WILL BE A MENU',
+    tips = []
+    tips.push(tippy('#sites', {
+        content: `<ul class = "dropdown"><li class = "dropdown-item"><a href="#" class = "dropdown-link">Ben's House</a></li><li class = "dropdown-item"><a href="#" class = "dropdown-link">Jen's House</a></li><li class = "dropdown-item"><a href="#" class = "dropdown-link">Luke's House</a></li></ul>`,
         duration: 0,
         placement: 'right',
-        intertia: true,
+      
         interactiveBorder: 20,
         offset: [0, 0],
         
         interactive: true,
         allowHTML: true
+    })[0]);
+    tips.push(tippy('#metric-icon', {
+        content: `<ul class = "dropdown"><li class = "dropdown-item"><a href="#" class = "dropdown-link">Temperature</a></li><li class = "dropdown-item"><a href="#" class = "dropdown-link">Humidity</a></li><li class = "dropdown-item"><a href="#" class = "dropdown-link">Wind Speed</a></li><li class = "dropdown-item"><a href="#" class = "dropdown-link">Wind Direction</a></li></ul>`,
+        duration: 0,
+        placement: 'right',
+
+        interactiveBorder: 20,
+        offset: [0, 0],
+
+        interactive: true,
+        allowHTML: true
+    })[0]);
+    const singleton = tippy.createSingleton(tips, { 
+        delay: 50, 
+        allowHTML:true, 
+        placement:'right', 
+        interactive: true,
+        interactiveBorder: 35,
+        offset: [0,0]
     });
 });
